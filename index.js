5b943cdc1be75978a8571feef3c16aa1e2835b6c
AnshulBruh <prajapatanshul67@gmail.com>
2026-04-10 19:05:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const scoreIncrement = document.querySelector("#score-increment")
+const scoreCard = document.querySelector("#homeScoreCard")
+const homeScoreCard = document.querySelector("#homeScoreCard")
+const One = document.querySelector("#one")
+
+homeScoreCard.innerHTML = `
+    <h1>12</h1>
+`
+One = () => {
+    console.log("HOla")
+}
